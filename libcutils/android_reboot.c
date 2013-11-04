dd8026ae549f1141c1c181e285d9f4a5559a72be
Tom Marshall <tdm.code@gmail.com>
2013-11-04 23:49:44 +0000

    Revert "RECOVERY_PRE_COMMAND_CLEAR_REASON and extended reboot arguments"
    This reverts commit aa64ac2a523691cc3816437b87c5aa0aa20068a7.
    
    Conflicts:
    
    	toolbox/Android.mk
    
    Change-Id: I95fdeb500b1d8b35240e3aa5fbcd3387491be171

--- libcutils/android_reboot.c
+++ libcutils/android_reboot.c
@@ -20,7 +20,6 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 
 #include <cutils/android_reboot.h>
@@ -104,16 +103,12 @@ static void remount_ro(void)
 
 int android_reboot(int cmd, int flags, char *arg)
 {
-    int ret = 0;
-    int reason = -1;
+    int ret;
 
 #ifdef RECOVERY_PRE_COMMAND
-    if (cmd == (int) ANDROID_RB_RESTART2) {
-        if (arg && strlen(arg) > 0) {
-            char cmd[PATH_MAX];
-            sprintf(cmd, RECOVERY_PRE_COMMAND " %s", arg);
-            system(cmd);
-        }
+    if (cmd == ANDROID_RB_RESTART2) {
+        if (!strncmp((char *)arg,"recovery",8))
+            system( RECOVERY_PRE_COMMAND );
     }
 #endif
 
@@ -122,30 +117,21 @@ int android_reboot(int cmd, int flags, char *arg)
 
     switch (cmd) {
         case ANDROID_RB_RESTART:
-            reason = RB_AUTOBOOT;
+            ret = reboot(RB_AUTOBOOT);
             break;
 
         case ANDROID_RB_POWEROFF:
             ret = reboot(RB_POWER_OFF);
-            return ret;
+            break;
 
         case ANDROID_RB_RESTART2:
-            // REBOOT_MAGIC
+            ret = __reboot(LINUX_REBOOT_MAGIC1, LINUX_REBOOT_MAGIC2,
+                           LINUX_REBOOT_CMD_RESTART2, arg);
             break;
 
         default:
-            return -1;
+            ret = -1;
     }
-
-#ifdef RECOVERY_PRE_COMMAND_CLEAR_REASON
-    reason = RB_AUTOBOOT;
-#endif
-
-    if (reason != -1)
-        ret = reboot(reason);
-    else
-        ret = __reboot(LINUX_REBOOT_MAGIC1, LINUX_REBOOT_MAGIC2,
-                           LINUX_REBOOT_CMD_RESTART2, arg);
 
     return ret;
 }
