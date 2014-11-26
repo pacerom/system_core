redmi2/android_system_core

system/core: Add flag for HDMI compressed passthrough
Add audio output flags to support compressed passthrough over HDMI
for DD/DDP contents.

Change-Id: I308eca2c250591e3e3d6d6d28b6f67440ae3310d

include/system/audio.h
@@ -496,7 +496,9 @@ typedef enum {
     AUDIO_OUTPUT_FLAG_VOIP_RX = 0x4000,  // use this flag in combination with DIRECT to
                                          // indicate HAL to activate EC & NS
                                          // path for VOIP calls
-    AUDIO_OUTPUT_FLAG_INCALL_MUSIC = 0x8000 //use this flag for incall music delivery
+    AUDIO_OUTPUT_FLAG_INCALL_MUSIC = 0x8000, //use this flag for incall music delivery
+    // flag for HDMI compressed passthrough
+    AUDIO_OUTPUT_FLAG_COMPRESS_PASSTHROUGH = 0x10000
 } audio_output_flags_t;
 
 /* The audio input flags are analogous to audio output flags.

pacerom/system_core

include/system/audio.h
@@ -783,7 +783,9 @@ typedef enum {
     AUDIO_OUTPUT_FLAG_VOIP_RX = 0x1000,  // use this flag in combination with DIRECT to
                                          // indicate HAL to activate EC & NS
                                          // path for VOIP calls
-    AUDIO_OUTPUT_FLAG_INCALL_MUSIC = 0x2000 //use this flag for incall music delivery
+    AUDIO_OUTPUT_FLAG_INCALL_MUSIC = 0x2000, //use this flag for incall music delivery
+    // flag for HDMI compressed passthrough
+    AUDIO_OUTPUT_FLAG_COMPRESS_PASSTHROUGH = 0x4000
 } audio_output_flags_t;
 
 /* The audio input flags are analogous to audio output flags.